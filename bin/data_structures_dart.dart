fec7305aabc0dbc3395239dd8cc5698627d94631
suzan <suzan@gmail.com>
2025-07-14 19:10:58 +0300

    Setup Dart console project structure with pubspec and bin/

---
+++ bin/data_structures_dart.dart
@@ -0,0 +1,5 @@
+import 'package:data_structures_dart/data_structures_dart.dart' as data_structures_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${data_structures_dart.calculate()}!');
+}
---
+++ test/data_structures_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:data_structures_dart/data_structures_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
